03ffeb321875f54737dd9cde0ff48c90a525ad72
Oscar More <oscarmore2@gmail.com>
2024-11-21 03:53:45 +0800

    add admin

--- apps/cryptoforward/admin.py
+++ apps/cryptoforward/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import TradingPair, ExchangeChannel, ExchangeOrder, ExchangeAccountInfo, ExcangeSignalTrading, DepositAccount
+from .models import TradingPair, ExchangeChannel, ExchangeOrder, ExchangeAccountInfo, ExcangeSignalTrading, DepositAccount, ExchangeConfig
 
 admin.site.register(TradingPair)
 admin.site.register(ExchangeChannel)
@@ -8,5 +8,6 @@ admin.site.register(ExchangeOrder)
 admin.site.register(ExchangeAccountInfo)
 admin.site.register(ExcangeSignalTrading)
 admin.site.register(DepositAccount)
+admin.site.register(ExchangeConfig)
 
 # Register your models here.
