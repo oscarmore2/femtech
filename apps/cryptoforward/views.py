88a73c868e6443ce0549eb19b87daa7c0ac43228
Oscar More <oscarmore2@gmail.com>
2024-11-22 02:55:01 +0800

    adjust

--- apps/cryptoforward/views.py
+++ apps/cryptoforward/views.py
@@ -148,7 +148,7 @@ def execute_account_trading(fingerPrint: str, accounts: object, context: object)
                 }
                 # 调用下单方法
                 response = exchange_api.place_order(
-                    trading_pair=fingerPrint,
+                    trading_pair=context["ticker"],
                     amount=context["amount"],
                     order_type="buy",  # 确保为小写
                     pos_side=context["direction"]  # 默认使用 LONG 或根据需要修改
