7896aebedd88ad26893f2e68fa77295ed8fef0e8
Oscar More <oscarmore2@gmail.com>
2024-11-23 12:37:33 +0800

    add bi selection

--- apps/cryptoforward/models.py
+++ apps/cryptoforward/models.py
@@ -29,6 +29,8 @@ class TradingPair (models.Model):
         
     finger_print = models.TextField(primary_key=True, blank=True, editable=False, verbose_name="指纹值")
     treading_pair_currency = models.CharField(max_length=200, verbose_name="交易对币种")
+    target_currency = models.CharField(max_length=200, default="BTC", verbose_name="基准币种")
+    source_currency = models.CharField(max_length=200, default="USDT", verbose_name="基准币种")
     trading_context = models.TextField(blank=True, verbose_name="交易信息Context(json 格式)")
 
     def save(self, **kwargs):
@@ -39,7 +41,7 @@ class TradingPair (models.Model):
         super().save(**kwargs)
 
     def __str__(self):
-        return "{0}-{1}".format(self.treading_pair_currency, self.finger_print)
+        return "{0}-{1}-{2}".format(self.treading_pair_currency, self.finger_print)
 
 class ExchangeChannel(models.Model):
     id = models.AutoField(primary_key=True)
